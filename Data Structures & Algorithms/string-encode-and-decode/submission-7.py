779acce72639f4b43c89fd2ed7649fe72ea211b3
Daniel Portnov <danny.portnov@gmail.com>
2026-06-16 13:29:45 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    indexed = {}
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+
+        for i in range(len(strs)):
+            self.indexed[i] = strs[i]
+            res += str(i)
+            res += " "
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        indicies = s.split(" ")[:-1]
+
+        for index in indicies:
+            res.append(self.indexed[int(index)])
+
+        return res
